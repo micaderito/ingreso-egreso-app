f82e0ad0893d40b8ccc4d4ed11b2b408204c09da
Micaela De Rito <micaeladerito@gmail.com>
2022-06-15 00:25:26 -0300

    micash inicial

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/ingresoEgresoApp'),
+      dir: require('path').join(__dirname, './coverage/micash'),
       subdir: '.',
       reporters: [
         { type: 'html' },
